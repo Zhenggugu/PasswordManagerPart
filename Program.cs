e5690dc0b1eb0510facdad621e2c7f8d4d906068
志文 郑 <2312391811@qq.com>
2022-06-08 20:04:40 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace PasswordManagerPart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Reuse reuse = new();
+            reuse.Reusing();
+
+        }
+    }
+    public class Reuse
+    {
+
+        public int Reusing()
+        {
+            int count = 0;
+            string conStr = "server=localhost;user=root;password=123456;database=software";
+            MySqlConnection con = new MySqlConnection(conStr);
+            con.Open();
+            MySqlCommand command = new MySqlCommand();
+            command.Connection = con;
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = "SELECT DISTINCT PASS FROM passw;";
+            MySqlDataReader mySqlDataReader = command.ExecuteReader();
+            List<string> password = new();
+            while(mySqlDataReader.Read())
+            {
+                password.Add(mySqlDataReader.GetString(0));
+                //command.CommandText = "SELECT COUNT(*) FROM passw WHERE PASS=" + ;
+                //MySqlDataReader reader1 = command.ExecuteReader();
+                //count += reader1.GetInt32(0);
+            }
+            mySqlDataReader.Close();
+            foreach (var item in password)
+            {
+                command.CommandText = "SELECT COUNT(*) FROM passw WHERE PASS=" + item;
+                MySqlDataReader reader1 = command.ExecuteReader();
+                reader1.Read();
+                count += reader1.GetInt32(0);
+                reader1.Close();
+            }
+            Console.WriteLine("重用密码数量"+count);
+            con.Close();
+            return 0;
+        }
+    }
+}
+
